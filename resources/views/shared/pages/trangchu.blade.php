b9eaab90f1414702f3b649b6051be05bb5c8c9e2
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2020-04-28 06:20:41 +0700

    Update trangchu.blade.php

--- resources/views/shared/pages/trangchu.blade.php
+++ resources/views/shared/pages/trangchu.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
     @include('shared.layouts.slide')
-    <div class="container content">
+    <div class="container-fluid content">
 		<div class="row mt-3">
 			{{--  Noi dung chinh  --}}
 			<div class="col-md-9 col-lg-9">
